75df9f70f763d0b3b0d594bc0ccbb393202bc5e0
Averjan <timophey.krut@gmail.com>
2021-02-21 17:16:35 +0300

    Init commit

--- userAuth/Program.cs
+++ userAuth/Program.cs
@@ -14,7 +14,7 @@ namespace userAuth
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
